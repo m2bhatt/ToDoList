a6bde70316e2bc6975e34a02a0811527774abc8d
Megha Bhatt <megha.bhatt@shopify.com>
2017-11-22 09:58:37 -0500

    Display the list

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ function addItem(form){
   var input = form.children[0];
   var todo = input.value;
   items.push(todo);
+  var lenItem = items.length;
+  for (var i=0; i<=lenItem; i++){
+    lenItem = lenItem + i;
+  }
+  document.getElementById("firstList").innerHTML = items[i];
 };
