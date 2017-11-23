94aaff62b237ab63b9749d3663421ce7d88c176b
Megha Bhatt <megha.bhatt@shopify.com>
2017-11-22 23:08:17 -0500

    Display the entered text in an array

--- script.js
+++ script.js
@@ -5,9 +5,5 @@ function addItem(form){
   var input = form.children[0];
   var todo = input.value;
   items.push(todo);
-  var lenItem = items.length;
-  for (var i=0; i<=lenItem; i++){
-    lenItem = lenItem + i;
-  }
-  document.getElementById("firstList").innerHTML = items[i];
+  document.getElementById("firstList").innerHTML = items;
 };
