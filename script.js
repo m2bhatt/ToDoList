9b3543fb7fde60de6cd79af691988856e1d09c2e
Megha Bhatt <megha.bhatt@shopify.com>
2017-11-30 17:41:18 -0500

    Successfully adds items to a list

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //Add item to a list
 var items = [];
-
+//Why do we add form in brackets? What's the difference between empty brackets
+//and no brackets at all?
 function addItem(form){
-  var input = form.children[0];
-  var todo = input.value;
-  items.push(todo);
-  document.getElementById("firstList").innerHTML = items;
+  var input = form.children[0].value;
+  var test = document.createElement("li");
+  var text = document.createTextNode(input);
+  test.appendChild(text);
+  document.body.appendChild(test);
+  form.reset();
 };
