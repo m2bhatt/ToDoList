6da62aff76786c0d16536b087064659a15afb7c5
Megha Bhatt <megha.bhatt@shopify.com>
2017-12-12 17:24:25 -0500

    Added second version of addItem

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 //Add item to a list
-var items = [];
+//var items = [];
 //Why do we add form in brackets? What's the difference between empty brackets
 //and no brackets at all?
 function addItem(form){
   var input = form.children[0].value;
   var listItem = document.createElement("li");
   var text = document.createTextNode(input);
+  listItem.appendChild(text);
+  var list = document.getElementsByTagName('ul')[0];
+  list.appendChild(listItem);
+  form.reset();
+};
+
+function addItem2(form){
+  // takes user input
+  var input = takeUserInput(); //calls function
+  var item = form.children[0].value;
+  // displays it as a list item
+  updateList(); //function call
+  var listItem = document.createElement("li");
+  var text = document.createTextNode(item);
+  listItem.appendChild(text);
+  var list = document.getElementsByTagName('ul')[0];
+  list.appendChild(listItem);
+  // clears the field
+  form.reset(); // if call function on an object, refer to it as a method
+};
 
 //  var check = document.createElement("input");
 //  check.setAttribute("type", "checkbox");
 // check.type = "checkbox";
-
-  listItem.appendChild(text);
-  document.getElementsByTagName('ul')[0].appendChild(listItem);
-  form.reset();
-};
