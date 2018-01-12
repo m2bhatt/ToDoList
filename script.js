42b563ce9e0f36aa539100b89db3a8ef1ed9b3ad
Megha Bhatt <megha.bhatt@shopify.com>
2018-01-12 17:17:05 -0500

    Cleaned up the code and wrote helper functions, takeUserInput and addItem

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-//Add item to a list
-//var items = [];
-//Why do we add form in brackets? What's the difference between empty brackets
-//and no brackets at all?
-function addItem(form){
-  var input = form.children[0].value;
-  var listItem = document.createElement("li");
-  var text = document.createTextNode(input);
-  listItem.appendChild(text);
-//  var button = document.createElement("button");
-//  button.setAttribute("type", "checkbox");
-  var list = document.getElementsByTagName('ul')[0];
-  //list.appendChild(button);
-  list.appendChild(listItem);
-  form.reset();
-};
-
-// function checkList(){
-//   //var str = document.getElementById("myList").innerHTML;
-//   console.log(str[0]);
-// }
-
-
 function createTodo(form){
   var item = takeUserInput(form);
   addItem(item);
@@ -39,11 +16,3 @@ function addItem(item){ //function call
   var list = document.getElementsByTagName('ul')[0];
   list.appendChild(listItem);
 }
-
-
-var str = "Hello".strikethrough;
-console.log(str);
-
-//  var check = document.createElement("input");
-//  check.setAttribute("type", "checkbox");
-// check.type = "checkbox";
