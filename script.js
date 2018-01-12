5b2af04d5e1676d40a183d3047a882f41c26687d
Megha Bhatt <megha.bhatt@shopify.com>
2018-01-12 17:15:39 -0500

    Changes

--- script.js
+++ script.js
@@ -7,25 +7,42 @@ function addItem(form){
   var listItem = document.createElement("li");
   var text = document.createTextNode(input);
   listItem.appendChild(text);
+//  var button = document.createElement("button");
+//  button.setAttribute("type", "checkbox");
   var list = document.getElementsByTagName('ul')[0];
+  //list.appendChild(button);
   list.appendChild(listItem);
   form.reset();
 };
 
-function addItem2(form){
-  // takes user input
-  var input = takeUserInput(); //calls function
-  var item = form.children[0].value;
-  // displays it as a list item
-  updateList(); //function call
+// function checkList(){
+//   //var str = document.getElementById("myList").innerHTML;
+//   console.log(str[0]);
+// }
+
+
+function createTodo(form){
+  var item = takeUserInput(form);
+  addItem(item);
+  // clears the field
+  form.reset(); // if call function on an object, refer to it as a method
+};
+
+function takeUserInput(form) { //calls function
+  return form.children[0].value;
+}
+
+function addItem(item){ //function call
   var listItem = document.createElement("li");
   var text = document.createTextNode(item);
   listItem.appendChild(text);
   var list = document.getElementsByTagName('ul')[0];
   list.appendChild(listItem);
-  // clears the field
-  form.reset(); // if call function on an object, refer to it as a method
-};
+}
+
+
+var str = "Hello".strikethrough;
+console.log(str);
 
 //  var check = document.createElement("input");
 //  check.setAttribute("type", "checkbox");
