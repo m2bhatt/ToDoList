c645fccd3a5aec8fc58c229af7ae679016cd2a8b
Megha Bhatt <megha.bhatt@shopify.com>
2018-03-08 18:43:35 -0500

    Added local storage

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var tasks = [];
+
 function createTodo(form){
   var item = takeUserInput(form);
   addItem(item);
@@ -9,15 +11,27 @@ function takeUserInput(form) { //calls function
   return form.children[0].value;
 }
 
-function addItem(item){ //function call
+function addItem(taskDescription){ //function call
+  tasks.push(taskDescription);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
   var listItem = document.createElement("li");
-  var text = document.createTextNode(item);
+  var text = document.createTextNode(taskDescription);
   listItem.appendChild(text);
   var list = document.getElementsByTagName("ul")[0];
   listItem.setAttribute("class", "normalText");
   listItem.addEventListener("click", toggleDone);
   list.appendChild(listItem);
 }
+
+function retrieveList(){
+  var savedTasks =  JSON.parse(localStorage.getItem("tasks"));
+
+  for (i = 0; i < savedTasks.length; i++) {
+    addItem(savedTasks[i]);
+  }
+}
+
 
 function deleteList(){
   var delText = document.getElementById("myList");
