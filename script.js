f14f3e92558dba4e7caf1d970f6d912b02b4f1a6
Megha Bhatt <megha.bhatt@shopify.com>
2018-03-19 21:16:31 -0400

    Retrieve lists from local storage

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var tasks = [];
+var tasks = {
+  urgentImportant: [],
+  nonUrgentImportant: [],
+  urgentNotImportant: [],
+  notUrgentNotImportant: []
+};
 
 function createTodo(form){
   var item = takeUserInput(form);
-  addItem(item);
+  var list = form.nextElementSibling;
+  addItem(item, list);
   // clears the field
   form.reset(); // if call function on an object, refer to it as a method
 };
@@ -11,14 +17,25 @@ function takeUserInput(form) { //calls function
   return form.children[0].value;
 }
 
-function addItem(taskDescription){ //function call
-  tasks.push(taskDescription);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+function addItem(taskDescription, list){ //function call
+  var listIdentifier = list.parentElement.id;
+  storeItem(taskDescription, tasks[listIdentifier]);
+  syncWithLocalStorage();
+  renderItem(list, taskDescription);
+}
 
+function storeItem(content, tasks) {
+  tasks.push(content);
+}
+
+function syncWithLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderItem(list, content) {
   var listItem = document.createElement("li");
-  var text = document.createTextNode(taskDescription);
+  var text = document.createTextNode(content);
   listItem.appendChild(text);
-  var list = document.getElementsByTagName("ul")[0];
   listItem.setAttribute("class", "normalText");
   listItem.addEventListener("click", toggleDone);
   list.appendChild(listItem);
@@ -26,18 +43,31 @@ function addItem(taskDescription){ //function call
 
 function retrieveList(){
   var savedTasks =  JSON.parse(localStorage.getItem("tasks"));
+  // [ urgentImportant, urgentNotImportant ... ]
+  for (var key in savedTasks) {
+    // getElementbyId(key) retrieves parent div
+    list = document.getElementById(key).getElementsByTagName("ul")[0]
 
-  for (i = 0; i < savedTasks.length; i++) {
-    addItem(savedTasks[i]);
+    for (var i = 0; i < savedTasks[key].length; i++) {
+      addItem(savedTasks[key][i], list);
+    }
   }
 }
-
+  //document.getElementById(listOfKeys[i]).value;
+//   for (i = 0; i < savedTasks.length; i++){
+//     addItem(savedTasks[i]);
+//   }
+  // For every list in savedTasks:
+  // - Find the right ul DOM node
+  // - For every item in the particular arrays:
+  //   - invoke addItem
+//}
 
 function deleteList(){
-  var delText = document.getElementById("myList");
-  while (delText.firstChild){
-    delText.removeChild(delText.firstChild);
-    localStorage.removeItem("tasks");
+  var listIdentifier = list.parentElement.id;
+  while (listIdentifier.firstChild){
+    listIdentifier.removeChild(listIdentifier.firstChild);
+    localStorage.removeItem(tasks[listIdentifier]);
   }
 }
 
